491ec08e24165f114df7cd87cd8020508af0f104
Nikhil <solankinikhil74@gmail.com>
2023-11-02 13:16:06 +0530

    build update profile controller

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const {getDoctorInfoController} = require("../controllers/doctorCtrl");
+const {getDoctorInfoController, updateProfileController} = require("../controllers/doctorCtrl");
 const auth = require("../middlewares/auth");
 const router = express.Router();
 
 //POST SINGLE DOC INFO
 router.post("/getDoctorInfo", auth, getDoctorInfoController);
 
+router.post("/updateProfile" , auth ,  updateProfileController);
 
 
 module.exports = router;
