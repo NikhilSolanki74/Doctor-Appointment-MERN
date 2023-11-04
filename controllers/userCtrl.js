2477cb14b436d0f9cf26eabeec4e0917371c0004
Nikhil <solankinikhil74@gmail.com>
2023-11-04 12:15:39 +0530

    implement bookingAvailabilityController for checking the availability

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,38 +1,48 @@
-const bcrypt = require('bcryptjs')
-const userModel = require('../models/userModel')
-const doctorModel = require('../models/doctorModel')
-const jwt = require('jsonwebtoken')
-const appointmentModel = require('../models/appointmentModel')
-const registerController = async (req,res)=>{
-    try { 
-      const existingUser=await userModel.findOne({email:req.body.email});
-      if(existingUser){
-        return res.status(200).send({message:"User Already Exist" , success:false});
-      }
-      const password = req.body.password
-      const salt =await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password , salt);
-      req.body.password = hashedPassword;
-      const newUser = new userModel(req.body)
-        await newUser.save();
-        res.status(200).send({success:true , message:"Data Successfully Stored sir ji"})
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({success:false , message:`Sir the error in Controller is ${error.message}`})
-    }
-}
+const bcrypt = require("bcryptjs");
+const userModel = require("../models/userModel");
+const doctorModel = require("../models/doctorModel");
+const jwt = require("jsonwebtoken");
+const appointmentModel = require("../models/appointmentModel");
+const moment = require("moment");
 
-
-const loginController = async (req, res)=>{
-try {
-     const user = await userModel.findOne({email:req.body.email});
-     if(!user){
-        return res.status(200).send({
-          success:false,
-            message:"id Banalo Pehle"
-        })
+const registerController = async (req, res) => {
+  try {
+    const existingUser = await userModel.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res
+        .status(200)
+        .send({ message: "User Already Exist", success: false });
     }
-  
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newUser = new userModel(req.body);
+    await newUser.save();
+    res
+      .status(200)
+      .send({ success: true, message: "Data Successfully Stored sir ji" });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({
+        success: false,
+        message: `Sir the error in Controller is ${error.message}`,
+      });
+  }
+};
+
+const loginController = async (req, res) => {
+  try {
+    const user = await userModel.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(200).send({
+        success: false,
+        message: "id Banalo Pehle",
+      });
+    }
+
     const pass = await bcrypt.compare(req.body.password, user.password);
     if (!pass) {
       // Use res to send an error response
@@ -51,40 +61,39 @@ try {
       success: true,
       token,
     });
-
-} catch (error) {
+  } catch (error) {
     console.log(error);
     res.status(500).send({
-        success:false ,
-        message:"kuch to gadbad hai userCtrl.js  me bro"
-    })
-}
-}
+      success: false,
+      message: "kuch to gadbad hai userCtrl.js  me bro",
+    });
+  }
+};
 
-const authController = async (req , res)=>{
-try{
-   const user = await userModel.findById({_id:req.body.userId})
-   user.password=undefined;
-   if(!user){
-    return res.status(200).send({
-      message:'user not found',
-      success:false
-    })
-   }else{
-     res.status(200).send({
-      success:true,
-      data:user
-     })
-   }
-}catch(error){
-   console.log(error)
-   res.status(500).send({
-    message:'auth error bro',
-    success:false,
-    error
-   })
-}
-}
+const authController = async (req, res) => {
+  try {
+    const user = await userModel.findById({ _id: req.body.userId });
+    user.password = undefined;
+    if (!user) {
+      return res.status(200).send({
+        message: "user not found",
+        success: false,
+      });
+    } else {
+      res.status(200).send({
+        success: true,
+        data: user,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "auth error bro",
+      success: false,
+      error,
+    });
+  }
+};
 
 const applyDoctorController = async (req, res) => {
   try {
@@ -116,7 +125,6 @@ const applyDoctorController = async (req, res) => {
   }
 };
 
-
 const getAllNotificationController = async (req, res) => {
   try {
     const user = await userModel.findOne({ _id: req.body.userId });
@@ -141,8 +149,7 @@ const getAllNotificationController = async (req, res) => {
   }
 };
 
-
-const deleteAllNotificationController=async (req,res)=>{
+const deleteAllNotificationController = async (req, res) => {
   try {
     const user = await userModel.findOne({ _id: req.body.userId });
     user.notifcation = [];
@@ -184,6 +191,8 @@ const getAllDoctorsController = async (req, res) => {
 
 const bookAppointmentController = async (req, res) => {
   try {
+    req.body.date = moment(req.body.date, "DD-MM-YYYY").toISOString();
+    req.body.time = moment(req.body.time, "HH:mm").toISOString();
     req.body.status = "pending";
     const newAppointment = new appointmentModel(req.body);
     await newAppointment.save();
@@ -209,4 +218,51 @@ const bookAppointmentController = async (req, res) => {
 };
 
 
-module.exports = {loginController, registerController , authController, applyDoctorController , getAllNotificationController ,deleteAllNotificationController ,getAllDoctorsController,bookAppointmentController  };
+const bookingAvailabilityController = async (req, res) => {
+  try {
+    const date = moment(req.body.date, "DD-MM-YY").toISOString();
+    const fromTime = moment(req.body.time, "HH:mm")
+      .subtract(1, "hours")
+      .toISOString();
+    const toTime = moment(req.body.time, "HH:mm").add(1, "hours").toISOString();
+    const doctorId = req.body.doctorId;
+    const appointments = await appointmentModel.find({
+      doctorId,
+      date,
+      time: {
+        $gte: fromTime,
+        $lte: toTime,
+      },
+    });
+    if (appointments.length > 0) {
+      return res.status(200).send({
+        message: "Appointments not Availibale at this time",
+        success: true,
+      });
+    } else {
+      return res.status(200).send({
+        success: true,
+        message: "Appointments available",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error In Booking",
+    });
+  }
+};
+
+module.exports = {
+  loginController,
+  registerController,
+  authController,
+  applyDoctorController,
+  getAllNotificationController,
+  deleteAllNotificationController,
+  getAllDoctorsController,
+  bookAppointmentController,
+  bookingAvailabilityController
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const { loginController, registerController, authController,applyDoctorController,getAllNotificationController,deleteAllNotificationController, getAllDoctorsController, bookAppointmentController } = require('../controllers/userCtrl');
-const auth = require('../middlewares/auth');
+const express = require("express");
+const {
+  loginController,
+  registerController,
+  authController,
+  applyDoctorController,
+  getAllNotificationController,
+  deleteAllNotificationController,
+  getAllDoctorsController,
+  bookAppointmentController,
+  bookingAvailabilityController,
+} = require("../controllers/userCtrl");
+const auth = require("../middlewares/auth");
 const router = express.Router();
 
-router.post('/login' , loginController)
-router.post('/register' , registerController);
+router.post("/login", loginController);
+router.post("/register", registerController);
 
+router.post("/getUserData", auth, authController);
 
-router.post('/getUserData' , auth ,authController)
+router.post("/apply-doctor", auth, applyDoctorController);
 
+router.post("/get-all-notification", auth, getAllNotificationController);
 
-
-router.post('/apply-doctor' , auth ,applyDoctorController)
-
-router.post('/get-all-notification' , auth ,getAllNotificationController)
-
-router.post('/delete-all-notification' , auth ,deleteAllNotificationController)
+router.post("/delete-all-notification", auth, deleteAllNotificationController);
 
 router.get("/getAllDoctors", auth, getAllDoctorsController);
 
-router.post("/book-appointment" , auth , bookAppointmentController);
+router.post("/book-appointment", auth, bookAppointmentController);
 
+router.post("/booking-availability", auth, bookingAvailabilityController);
 
-module.exports = router
+module.exports = router;
